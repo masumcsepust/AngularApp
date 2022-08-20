55bb4b7f462c4343b33be2e42eefe4c390183312
masumcsepust <masumcsepust@example.com>
2022-08-20 16:37:51 +0600

    cxvxc

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/dist/angular-heroku-demo'));
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname+ '/dist/angular-heroku-demo')));
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-app-heroku'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
