dcccfe4aead7b131e1372e2690ff52e5c00fdc59
masumcsepust <masumcsepust@example.com>
2022-08-20 17:31:09 +0600

    xcvx

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/nodemodules'));
+app.get('/*', function(req, res) {
+    res.sendFile(
+    'index.html', {root: 'dist/nodemodules/'}
+  );
+  });
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+  app.listen(process.env.PORT || 8080);
