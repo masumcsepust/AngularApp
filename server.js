53a767bb26f6495b6b904c7300c1a142d2ae3a8a
masumcsepust <masumcsepust@example.com>
2022-08-21 10:50:00 +0600

    server add successfully

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-const express = require('express');
-const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/nodemodules'));
-app.get('/*', function(req, res) {
-    res.sendFile(
-    'index.html', {root: 'dist/nodemodules/'}
-  );
-  });
+let express = require('express');
 
-  app.listen(process.env.PORT || 8080);
+let app = express();
+
+app.use(express.static(__dirname+'/dist/selise-masum-task'));
+
+app.get('/*', (req,res) => {
+    res.sendFile(__dirname+'/dist/selise-masum-task/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
